b4be75067bddb960d1a5aef400dfbad1b48f93c5
Freddynnn <freddy.tnn@gmail.com>
2024-06-18 15:23:45 +1000

    adding watchlist button to homepage

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 import './index.scss';
 import AnimateLetters from '../AnimatedLetters';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlusCircle, faMagnifyingGlass, faPlus, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faPlusCircle, faMagnifyingGlass, faPlus, faUser, faListAlt, faListCheck } from '@fortawesome/free-solid-svg-icons';
 import axios from 'axios'; // Import axios
 
 const Home = ({isLoggedIn}) => {
@@ -34,7 +34,7 @@ const Home = ({isLoggedIn}) => {
                 <h2>As always, dedicated to my love Jo </h2>
                 <br/>
                 <ul>
-                    <li className='half'>
+                    <li className='third'>
 
                         {isLoggedIn 
                             ? <Link to="/new" className='flat-button add' data-after-text="ADD ENTRY">
@@ -46,12 +46,22 @@ const Home = ({isLoggedIn}) => {
                         }
                         
                     </li>
-                    <li className='half'>
+                    <li className='third'>
                         {isLoggedIn 
                             ? <Link to="/search" className='flat-button find' data-after-text="SEARCH FOR MEDIA">
                                 <FontAwesomeIcon icon={faMagnifyingGlass} />
                             </Link> 
                             : <Link to="/register" className='flat-button register' data-after-text="SIGN UP FOR FJORD">
+                                <FontAwesomeIcon icon={faPlus} />
+                            </Link>
+                        }
+                    </li>
+                    <li className='third'>
+                        {isLoggedIn 
+                            ? <Link to="/watchlist" className='flat-button find' data-after-text="ADD TO WATCHLIST">
+                                <FontAwesomeIcon icon={faListCheck} />
+                            </Link> 
+                            : <Link to="/watchlist" className='flat-button register' data-after-text="ADD TO WATCHLIST">
                                 <FontAwesomeIcon icon={faPlus} />
                             </Link>
                         }
